3c9a8f5908b7a0a4c065fc4479b657c22d37112f
CN <ckddnjs554@naver.com>
2021-08-26 22:25:58 +0900

    Feat : MainActivity property 설정 작업

--- app/src/main/java/com/yoochangwonspro/timealarmproject/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/timealarmproject/MainActivity.kt
@@ -2,8 +2,27 @@ package com.yoochangwonspro.timealarmproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val timeTextView: TextView by lazy {
+        findViewById(R.id.time_text_view)
+    }
+
+    private val amPmTextView: TextView by lazy {
+        findViewById(R.id.am_pm_text_view)
+    }
+
+    private val alarmChangeButton: Button by lazy {
+        findViewById(R.id.alarm_time_change_btn)
+    }
+
+    private val alarmOnOffButton: Button by lazy {
+        findViewById(R.id.alarm_on_off_btn)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
