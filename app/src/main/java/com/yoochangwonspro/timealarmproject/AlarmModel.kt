f08c7de640a810351ffa906dabaeda812b572ad7
CN <ckddnjs554@naver.com>
2021-08-26 22:19:48 +0900

    Feat : AlarmModel data class 의 timeText 변수 설정

--- app/src/main/java/com/yoochangwonspro/timealarmproject/AlarmModel.kt
+++ app/src/main/java/com/yoochangwonspro/timealarmproject/AlarmModel.kt
@@ -5,6 +5,14 @@ data class AlarmModel(
     val minute: Int,
     val onOff: Boolean
 ) {
+    val timeText: String
+        get() {
+            val h = "%02d".format(if (hour > 12) hour - 12 else hour)
+            val m = "%02d".format(minute)
+
+            return "$h:$m"
+        }
+
     val timeAmPm: String
         get() {
             return if (hour < 12) "AM" else "PM"
