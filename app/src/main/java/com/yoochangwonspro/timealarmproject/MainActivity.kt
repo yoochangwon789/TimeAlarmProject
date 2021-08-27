048467cb9103d4089d41f2ca6ad3f9ddb61a3632
CN <ckddnjs554@naver.com>
2021-08-27 19:23:26 +0900

    Feat : MainActivity onCreate 함수의 모든 함수 호출

--- app/src/main/java/com/yoochangwonspro/timealarmproject/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/timealarmproject/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initAlarmChangeButton()
+        initAlarmOnOffButton()
 
         val model = fetchDataAlarm()
         renderView(model)
