f6a911aaad551a29a10f6fe6b75ddd580bf5368f
CN <ckddnjs554@naver.com>
2021-08-27 17:38:47 +0900

    Feat : AlarmModel 파라미터 onOff 변수 var 타입으로 수정

--- app/src/main/java/com/yoochangwonspro/timealarmproject/AlarmModel.kt
+++ app/src/main/java/com/yoochangwonspro/timealarmproject/AlarmModel.kt
@@ -3,7 +3,7 @@ package com.yoochangwonspro.timealarmproject
 data class AlarmModel(
     val hour: Int,
     val minute: Int,
-    val onOff: Boolean
+    var onOff: Boolean
 ) {
     val timeText: String
         get() {
